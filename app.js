f70d2a456656b0b3e99e6e4c376fe6e4591d045e
maximoortelli <maximoortelli@hotmail.com>
2024-05-12 22:08:13 -0300

    Update app.js
    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 // import { createStore } from " redux"
-let createStore = require('redux').createStore;
+const createStore = require('redux').createStore;
+const combineReducers = require('redux').combineReducers; //importamos combineReducers
 
 /* 
      Actions = los actions son acciones que se envian al store, en este caso para comprar un producto.
@@ -20,13 +21,20 @@ const buy_pokemon_action = (cant) => { // <-- Este es la accion de comprar un po
     }
 }
 
-const return_pokemon_action = (cant) => { // <-- Este es la accion de comprar un pokemon.
+const return_pokemon_action = (cant) => { // <-- Este es el TIPO DE ACCION de retornar un pokemon.
     return {
         type: RETURN_POKEMON,
         payload: cant
     }
 }
 
+const BUY_SWITCH = 'BUY_SWITCH';// <-- Este es el tipo para comprar una switch.
+const buy_switch_action = (cant) => { // <-- Este es la accion de comprar una switch.
+    return {
+        type: BUY_SWITCH,
+        payload: cant
+    }
+}
 // Reducers = Los reducers ordenan el almacen.
 const default_games_state = {
     pokemon: 10,
@@ -52,15 +60,40 @@ const games_reducer = (state = default_games_state, action) => {
     } 
 }
 
+const default_consoles_state = {
+    ps5: 30,
+    switch: 30
+}
+
+const consoles_reducer = (state = default_consoles_state, action) => {
+    switch(action.type){
+        case BUY_SWITCH: {
+            return {
+                ...state, // Devolvemos el estado anterior y hacemos la modificacion pertinente
+                pokemon: state.switch - action.payload
+            }
+        }
+
+        default: return state;
+    }
+}
+
+// rutas de reducers
+const rootReducers = combineReducers({// combina los reducers
+    games_reducer, // games reducers
+    consoles_reducer // consoles reducers
+});
+
 // Store = caja que almacena los Almacen de nuestra tienda.
-const store = createStore(games_reducer);
+const store = createStore(rootReducers);
 //store.getState() devuelve el estado inicial
 console.log('Estado Inicial:', store.getState());
 
 store.subscribe(() => {
     console.log('Cambio de estado:', store.getState());
 });
-
-// dispatch reparte los mensajes del store
-store.dispatch(buy_pokemon_action(3));
-store.dispatch(return_pokemon_action(2));
+// 
+// // dispatch reparte los mensajes del store
+// store.dispatch(buy_pokemon_action(3));
+// store.dispatch(return_pokemon_action(2));
+store.dispatch(buy_switch_action(20));
